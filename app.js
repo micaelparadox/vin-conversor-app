c192356631296413081712843ef26428ffa5ec8b
micaelparadox <micaelparadox@gmail.com>
2023-02-02 17:41:15 -0300

    cleaning the trash

--- app.js
+++ app.js
@@ -15,14 +15,3 @@ app.use("/", require("./routes/index"));
 
 // app.use(express.static("public"));
 
-const Car = require('./models/car');
-
-Car.create({
-    marca: 'Audi',
-    modelo: 'A3',
-    ano: 2019,
-    software: 'V1.0.0',
-    hardware: 'HW2.0',
-    longCoding: '70FB6BA1502021724777060260C9A90ED60A84E2600695773842F50952094B0B0000000012121212B835351919323200',
-    chassis: 'WAUBFGFF5G1099999'
-});
