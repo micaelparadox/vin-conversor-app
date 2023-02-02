9d0ac2066110df43de063097c8e223277066d4c7
micaelparadox <micaelparadox@gmail.com>
2023-02-02 14:12:12 -0300

    creating test cases

--- tests/processInput.test.js
+++ tests/processInput.test.js
@@ -8,3 +8,18 @@ describe('processInput', () => {
         expect(result.length).toBe(longCode.length);
     });
 });
+
+test("Long code and VIN match", () => {
+    const longCode = "70FB6BA1502021724777060260C9A90ED60A84E2600695773842F50952094B0B0000000012121212B835351919323200";
+    const vin = "WVWHE6AU4FW006026";
+
+    const result = processInput(longCode, vin);
+    expect(result.length).toBe(longCode.length + vin.length);
+});
+
+test("Long code and VIN do not match", () => {
+    const longCode = "70FB6BA1502021724777060260C9A90ED60A84E2600695773842F50952094B0B0000000012121212B835351919323200";
+    const vin = "WVWHE6AU4FW00602";
+
+    expect(() => processInput(longCode, vin)).toThrow();
+});
